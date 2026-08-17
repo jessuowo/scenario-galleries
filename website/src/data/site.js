019be5078091482b539bc78059e2351bb910a885
jessuowo <bubbapup22@gmail.com>
2026-08-17 01:06:23 +0000

    Update site branding

---
+++ website/src/data/site.js
@@ -0,0 +1,15 @@
+export const site = {
+  name: "Scenario Galleries",
+
+  tagline:
+    "Character artwork, locations, and visual extras from my FictionLab scenarios.",
+
+  creator: "jessuowo",
+
+  description:
+    "A collection of visual galleries for my FictionLab scenarios.",
+
+  links: {
+    fictionLab: "",
+  },
+};
